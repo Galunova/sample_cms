f91955331aef030d80026c3ed177151760abb257
Adelina Galunova <adelya@cloudcoreo.com>
2020-08-18 18:49:10 +0600

    Add CRUD for pages. Add validations on page creation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ApplicationRecord
 	belongs_to :index_template, class_name: 'Template', optional: true
 	belongs_to :show_template, class_name: 'Template', optional: true
+
+	validates :slug, presence: true, 
+									 uniqueness: true
 end
